38768f4d7990af45ac1277ea353e66e5c6b661af
Sn0w <garrettbeasley@icloud.com>
2014-10-01 18:08:00 -0700

    Init Commit

---
+++ src/Pick_A_Pet.java
@@ -0,0 +1,34 @@
+/**
+ * Screensaver.java
+ * @author Garrett J. Beasley
+ * 09/29/2014
+ * Draws a House Picture
+ */
+
+//Imports for the program.
+
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import java.awt.Color;
+import java.util.Random;
+
+
+public class Pick_A_Pet extends GraphicsProgram 
+{
+	public static void main(String[] args)
+	{
+		randomGen();
+		
+		
+		
+	}
+
+	private static void randomGen() 
+	{
+		
+		
+
+	}
+
+}
+
