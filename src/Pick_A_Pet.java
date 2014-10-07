aa113c882d51d2df0845e6a4e2f1546f69cd6a88
Sn0w <garrettbeasley@icloud.com>
2014-10-06 19:15:52 -0700

    Final Push off the completed program, uses the consoleprogram in the acm library

--- src/Pick_A_Pet.java
+++ src/Pick_A_Pet.java
@@ -7,27 +7,74 @@
 
 //Imports for the program.
 
-import acm.graphics.*;
-import acm.program.GraphicsProgram;
-import java.awt.Color;
-import java.util.Random;
+import acm.program.ConsoleProgram;
 
 
-public class Pick_A_Pet extends GraphicsProgram 
+public class Pick_A_Pet extends ConsoleProgram
 {
-	public static void main(String[] args)
+	
+	public void run()
 	{
-		randomGen();
+		/**Set the size of the applet*/
+		setSize(500,400);
 		
+		/**Gathers the inputs from the user and stores them in a variable*/
+		String name = readLine("Enter your name:");
+		String gender = readLine("Enter your gender, "+name+":");
+		int age = readInt("Enter your age, "+name+" "+gender+":");
 		
+		/**Checks the age if they are under 10 or over 80*/
+		if (age <10 || age >80)
+		{
+			print("You should get a fish, "+name+"!");
+		}
 		
-	}
+		/**Checks the age if they are under equal to or greater then 10 or less then or equal to 20*/
+		if (age >=10 && age <=20)
+		{
+			if (gender.equalsIgnoreCase("male"))
+			{
+				print("You should get a Turtle, "+name+"!");
+			}
+			else if (gender.equalsIgnoreCase("female")) 
+			{
+				print("You should get a frog, "+name+"!");
+			}
+			
+		}
+		
+		/**Checks the age if they are under equal to or greater then 21 or less then or equal to 50*/
+		else if (age >=21 && age <=50)
+		{
+			if (gender.equalsIgnoreCase("male"))
+			{
+				print("You should get a bulldog, "+name+"!");
+			}
+			else if (gender.equalsIgnoreCase("female")) 
+			{
+				print("You should get a alpaca, "+name+"!");
+			}
+			
+		}
+		
+		/**Checks the age if they are under equal to or greater then 51 or less then or equal to 80*/
+		else if (age >=51 && age <=80)
+		{
+			if (gender.equalsIgnoreCase("male"))
+			{
+				print("You should get a iguana, "+name+"!");
+			}
+			else if (gender.equalsIgnoreCase("female")) 
+			{
+				print("You should get a lizard, "+name+"!");
+			}
+			
+		}
 
-	private static void randomGen() 
-	{
-		
-		
 
+		
+		
+		
 	}
 
 }
